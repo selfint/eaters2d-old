e80c141b17410c498e7dfaf9f4a67d26d6473ccc
Tom Selfin <selfint@gmail.com>
2021-08-15 23:29:00 +0300

    REFACTOR: extract startup_camera system

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ fn main() {
     App::build()
         .insert_resource(ClearColor(Color::rgb(0.5, 0.45, 0.0)))
         .add_plugins(DefaultPlugins)
+        .add_startup_system(startup_camera.system())
         .add_startup_system(add_creatures.system())
         .run();
+}
+
+fn startup_camera(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
 
 fn add_creatures(
@@ -17,7 +22,6 @@ fn add_creatures(
 ) {
     let mut texture_handle: ColorMaterial = asset_server.load("white_circle.png").into();
     texture_handle.color = CREATURE_COLOR;
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(SpriteBundle {
         material: materials.add(texture_handle),
         sprite: Sprite::new(Vec2::new(50.0, 50.0)),
