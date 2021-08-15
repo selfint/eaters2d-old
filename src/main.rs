416e9e9437e0e7b45c04cab64203d0a4f9f4661e
Tom Selfin <selfint@gmail.com>
2021-08-15 18:36:16 +0300

    FEAT: set background color

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::build().add_plugins(DefaultPlugins).run();
+    App::build()
+        .insert_resource(ClearColor(Color::rgb(0.5, 0.45, 0.0)))
+        .add_plugins(DefaultPlugins)
+        .run();
 }
