1153eb9a924a3007a39a60a6bf7ebf06afaadafe
Tom Selfin <selfint@gmail.com>
2021-08-15 23:15:52 +0300

    FEAT: render white circle as creature

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,19 @@ fn main() {
     App::build()
         .insert_resource(ClearColor(Color::rgb(0.5, 0.45, 0.0)))
         .add_plugins(DefaultPlugins)
+        .add_startup_system(add_creatures.system())
         .run();
+}
+
+fn add_creatures(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let texture_handle = asset_server.load("white_circle.png");
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(texture_handle.into()),
+        ..Default::default()
+    });
 }
