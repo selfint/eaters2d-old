ed5530a25bce9d8a07a5edabda7de975ce21f81c
Tom Selfin <selfint@gmail.com>
2021-08-27 19:30:45 +0300

    RESET: revert to empty canvas

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,8 @@ use rand::Rng;
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
-const CREATURE_AMOUNT: usize = 10;
-const CREATURE_SIZE: f32 = 10.0;
-const CREATURE_PRIORITY: f32 = 2.;
-const FOOD_AMOUNT: usize = 10;
-const FOOD_SIZE: f32 = 5.0;
-const FOOD_PRIORITY: f32 = 1.;
-const WINDOW_WIDTH: f32 = 300.;
-const WINDOW_HEIGHT: f32 = 300.;
+const WINDOW_WIDTH: f32 = 500.;
+const WINDOW_HEIGHT: f32 = 500.;
 
 #[cfg(target_arch = "wasm32")]
 #[wasm_bindgen]
@@ -32,70 +26,10 @@ pub fn run() {
     #[cfg(target_arch = "wasm32")]
     app.add_plugin(bevy_webgl2::WebGL2Plugin);
 
-    app.add_startup_system(startup_camera.system())
-        .add_startup_system(add_creatures.system())
-        .add_startup_system(add_foods.system());
+    app.add_startup_system(startup_camera.system());
     app.run();
 }
 
 fn startup_camera(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-}
-
-fn add_creatures(
-    asset_server: Res<AssetServer>,
-    mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    let texture_handle: ColorMaterial = asset_server.load("red_circle.png").into();
-    let creature_material = materials.add(texture_handle);
-    let mut rng = rand::thread_rng();
-
-    let min_x = -WINDOW_WIDTH / 2.;
-    let max_x = min_x + WINDOW_WIDTH;
-    let min_y = -WINDOW_HEIGHT / 2.;
-    let max_y = min_y + WINDOW_HEIGHT;
-
-    for _ in 0..CREATURE_AMOUNT {
-        let creature = SpriteBundle {
-            material: creature_material.clone(),
-            sprite: Sprite::new(Vec2::new(CREATURE_SIZE, CREATURE_SIZE)),
-            transform: Transform::from_xyz(
-                rng.gen_range(min_x..max_x),
-                rng.gen_range(min_y..max_y),
-                CREATURE_PRIORITY,
-            ),
-            ..Default::default()
-        };
-        commands.spawn_bundle(creature);
-    }
-}
-
-fn add_foods(
-    asset_server: Res<AssetServer>,
-    mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    let texture_handle: ColorMaterial = asset_server.load("white_circle.png").into();
-    let creature_material = materials.add(texture_handle);
-    let mut rng = rand::thread_rng();
-
-    let min_x = -WINDOW_WIDTH / 2.;
-    let max_x = min_x + WINDOW_WIDTH;
-    let min_y = -WINDOW_HEIGHT / 2.;
-    let max_y = min_y + WINDOW_HEIGHT;
-
-    for _ in 0..FOOD_AMOUNT {
-        let food = SpriteBundle {
-            material: creature_material.clone(),
-            sprite: Sprite::new(Vec2::new(FOOD_SIZE, FOOD_SIZE)),
-            transform: Transform::from_xyz(
-                rng.gen_range(min_x..max_x),
-                rng.gen_range(min_y..max_y),
-                FOOD_PRIORITY,
-            ),
-            ..Default::default()
-        };
-        commands.spawn_bundle(food);
-    }
 }
