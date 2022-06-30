e7700fe1928f83b6e62d0dd774a34cf9b9db5571
Tom Selfin <selfint@gmail.com>
2022-06-30 22:20:28 +0300

    upgrade to bevy 0.7

--- src/creature.rs
+++ src/creature.rs
@@ -2,20 +2,22 @@ use bevy::prelude::*;
 
 const CREATURE_SIZE: f32 = 10.;
 
+#[derive(Component)]
 pub struct Creature;
 
 pub fn add_creatures(
     asset_server: Res<AssetServer>,
     mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let texture_handle: ColorMaterial = asset_server.load("red_circle.png").into();
-    let creature_material = materials.add(texture_handle);
 
     let creature = SpriteBundle {
-        material: creature_material.clone(),
-        sprite: Sprite::new(Vec2::new(CREATURE_SIZE, CREATURE_SIZE)),
+        sprite: Sprite { 
+            custom_size: Some(Vec2::new(CREATURE_SIZE, CREATURE_SIZE)), 
+            ..default()
+        },
+        texture: asset_server.load("red_circle.png"),
         ..Default::default()
     };
-    commands.spawn_bundle(creature);
+
+    commands.spawn_bundle(creature).insert(Creature);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use crate::creature::add_creatures;
 use bevy::prelude::*;
-#[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
 mod creature;
@@ -8,27 +7,26 @@ mod creature;
 const WINDOW_WIDTH: f32 = 500.;
 const WINDOW_HEIGHT: f32 = 500.;
 
-#[cfg(target_arch = "wasm32")]
 #[wasm_bindgen]
 pub fn run_web() {
     run();
 }
 
 pub fn run() {
-    let mut app = App::build();
+    let mut app = App::new();
+
     app.insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.5)));
     app.insert_resource(WindowDescriptor {
         width: WINDOW_WIDTH,
         height: WINDOW_HEIGHT,
         ..Default::default()
     });
+
     app.add_plugins(DefaultPlugins);
 
-    #[cfg(target_arch = "wasm32")]
-    app.add_plugin(bevy_webgl2::WebGL2Plugin);
+    app.add_startup_system(startup_camera)
+        .add_startup_system(add_creatures);
 
-    app.add_startup_system(startup_camera.system())
-        .add_startup_system(add_creatures.system());
     app.run();
 }
 
