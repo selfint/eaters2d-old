5e7a0ad20135af6f6b698b01d8b4203c6ea6a1d2
selfint <selfint@gmail.com>
2022-07-01 20:31:19 +0300

    dont compile bevy

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,37 @@
-use crate::creature::add_creatures;
-use bevy::prelude::*;
+// use crate::creature::add_creatures;
+// use bevy::prelude::*;
 use wasm_bindgen::prelude::*;
 
-mod creature;
+// mod creature;
 
-const WINDOW_WIDTH: f32 = 500.;
-const WINDOW_HEIGHT: f32 = 500.;
+// const WINDOW_WIDTH: f32 = 500.;
+// const WINDOW_HEIGHT: f32 = 500.;
 
 #[wasm_bindgen]
-pub fn run_web() {
-    run();
+pub fn run_web() -> JsValue {
+    run()
 }
 
-pub fn run() {
-    let mut app = App::new();
+pub fn run() -> JsValue {
+    "Hello from Rust!".into()
 
-    app.insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.5)));
-    app.insert_resource(WindowDescriptor {
-        width: WINDOW_WIDTH,
-        height: WINDOW_HEIGHT,
-        ..Default::default()
-    });
+    // let mut app = App::new();
 
-    app.add_plugins(DefaultPlugins);
+    // app.insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.5)));
+    // app.insert_resource(WindowDescriptor {
+    //     width: WINDOW_WIDTH,
+    //     height: WINDOW_HEIGHT,
+    //     ..Default::default()
+    // });
 
-    app.add_startup_system(startup_camera)
-        .add_startup_system(add_creatures);
+    // app.add_plugins(DefaultPlugins);
 
-    app.run();
+    // app.add_startup_system(startup_camera)
+    //     .add_startup_system(add_creatures);
+
+    // app.run();
 }
 
-fn startup_camera(mut commands: Commands) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-}
+// fn startup_camera(mut commands: Commands) {
+//     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+// }
